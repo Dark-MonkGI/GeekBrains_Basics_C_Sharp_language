83b89b31b72086a394f601a07ee5b23c028014f7
Dark-MonkGI <gryaznov774@gmail.com>
2022-08-07 23:05:59 +0400

    fist step

---
+++ LessonOne/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonOne
+{
+    internal class Person
+    {
+        private string Name;
+        private string Surname;
+        private int personAge;
+        private int height;
+        private int weight;
+
+        public Person()
+        {
+            WriteData();
+        }
+
+
+        private void WriteData()
+        {
+            WriteData("имя");
+            this.Name = Console.ReadLine();
+
+            WriteData("фамилию");
+            this.Surname = Console.ReadLine();
+
+            WriteData("возраст");
+            this.personAge = Convert.ToInt32(Console.ReadLine());
+
+            WriteData("Рост");
+            this.height = Convert.ToInt32(Console.ReadLine());
+
+            WriteData("вес");
+            this.weight = Convert.ToInt32(Console.ReadLine());
+        }
+
+
+
+        private void  WriteData(string parameter)
+        {
+            Console.WriteLine($"\n\rВведите {parameter}: ");
+        }
+
+        public void ShowMe()
+        {
+            string HiPerson = "Hello " + Name + " " + Surname;
+            string HiPersonNext = string.Format("Your age: {0} years old", personAge);
+            string HiPersonNextTwo = $"Your height: {height}, your weight: {weight}";
+
+            Console.WriteLine(HiPerson);
+            Console.WriteLine(HiPersonNext);
+            Console.WriteLine(HiPersonNextTwo);
+
+        }
+    }
+}
---
+++ LessonOne/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.ShowMe();
+            Console.ReadLine();
+        }
+
+    }
+}
