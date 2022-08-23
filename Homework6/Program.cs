7fca9c1a8dccbbae15176f05e6e0377044ac57d5
Грязнов Илья <iygryaznov@mts.ru>
2022-08-24 00:03:57 +0400

    Started sixth homework

--- Homework6/Program.cs
+++ Homework6/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SayHello;
 
 namespace Homework6
 {
@@ -10,7 +11,10 @@ namespace Homework6
     {
         static void Main(string[] args)
         {
+            Hello.Say(6);
 
+
+            Console.ReadLine();
         }
     }
 }
