95879e1b14439cd0024c8d1030e8c1a8fe135ec1
Грязнов Илья <iygryaznov@mts.ru>
2022-08-28 00:32:33 +0400

    Sixth homework second task

---
+++ Homework6/FindMin.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+
+namespace Homework6
+{
+    public delegate double Func(double x);
+
+    class FindMin
+    {
+        public static double Fone(double x)
+        {
+            return x * x - 50 * x + 10;
+        }
+
+        public static double Ftwo(double x)
+        {
+            return x * x - 10 * x + 50;
+        }
+
+        public void SaveFunc(Func F, string fileName, double x, double b, double h)
+        {
+            fileName = AppDomain.CurrentDomain.BaseDirectory + "..\\..\\" + fileName;
+
+            using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            {
+                using (BinaryWriter bw = new BinaryWriter(fs))
+                {
+                    while (x <= b)
+                    {
+                        bw.Write(F(x));
+                        x += h;
+                    }
+                }
+            }
+        }
+
+        public double Load(string fileName)
+        {
+            fileName = AppDomain.CurrentDomain.BaseDirectory + "..\\..\\" + fileName;
+            
+            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+            {
+                using(BinaryReader bw = new BinaryReader(fs))
+                {
+                    double min = double.MaxValue;
+                    double d;
+
+                    for (int i = 0; i < fs.Length / sizeof(double); i++)
+                    {
+                        // Считываем значение и переходим к следующему
+                        d = bw.ReadDouble();
+                        if (d < min) min = d;
+                    }
+                    return min;
+                }
+            }
+        }
+    }
+}
--- Homework6/Program.cs
+++ Homework6/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using SayHello;
@@ -20,11 +21,23 @@ namespace Homework6
             NewClass.Table(NewClass.MyFuncThree, -2, 2);
 
             Console.WriteLine("Таблица функции Sin:");
-            NewClass.Table(delegate (double x, double y ) { return y * Math.Sin(x); }, -2, 2);
+            NewClass.Table(delegate (double x, double y) { return y * Math.Sin(x); }, -2, 2);
 
             Console.WriteLine($"Для продолжения введите Enter");
             Console.ReadLine();
             Console.Clear();
+
+            //Task2
+            Hello.Say(6);
+
+            List<Func> findFunc = new List<Func>() { new Func(FindMin.Fone), new Func(FindMin.Ftwo) };
+
+            FindMin findMin = new FindMin();
+            Console.WriteLine("Введите число, соответствующее номеру желаемой функции:");
+            Console.WriteLine("\"1\" - Первая функци\n\r\"2\" - Вторая функция\n\r");
+
+            findMin.SaveFunc(findFunc[int.Parse(Console.ReadLine())-1], "data.bin", -100, 100, 0.5);
+            Console.WriteLine(findMin.Load("data.bin"));
 
             Console.ReadLine();
         }
